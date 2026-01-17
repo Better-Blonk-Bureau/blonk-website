7a874e0852509fa011afd8efdf4a06c47e0fd12b
Rhea Lin <rhea.lin8@gmail.com>
2026-01-17 02:22:20 -0500

    replace default app content with basic blonk content (#2)
    * remove default app stuff and add in pompompurin
    
    * add basic text about blonk, including contact email

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import pompompurinGif from './assets/pompompurin.gif'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <img src={pompompurinGif} className="pompompurin" alt="Pompompurin" />
+      <h1>blonk: a screen time app</h1>
+      <p>coming soon to a iOS device near you</p>
+      <p>for more info, email us at <a href="mailto:hello@blonk.app">hello@blonk.app</a></p>
+    </div>
   )
 }
 
