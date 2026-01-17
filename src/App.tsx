532df5ef27341f7c5f362614b2fa512284948daa
Rhea Lin <rhea.lin8@gmail.com>
2026-01-17 02:15:17 -0500

    add basic text about blonk, including contact email

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div>
       <img src={pompompurinGif} className="pompompurin" alt="Pompompurin" />
+      <h1>blonk: a screen time app</h1>
+      <p>coming soon to a iOS device near you</p>
+      <p>for more info, email us at <a href="mailto:hello@blonk.app">hello@blonk.app</a></p>
     </div>
   )
 }
